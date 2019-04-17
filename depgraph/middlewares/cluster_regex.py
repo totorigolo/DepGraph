07a8722eeece9732f633e2d158321396bcc6a01a
Thomas Lacroix <tholac@kth.se>
2019-04-17 16:27:27 +0200

    ClusterRegex: Now the list creates only one cluster

--- depgraph/middlewares/cluster_regex.py
+++ depgraph/middlewares/cluster_regex.py
@@ -28,24 +28,34 @@ class ClusterRegex(Middleware):
 
     def transform(self, dep_graph: DependencyGraph) \
             -> DependencyGraph:
+        logger.info('Clustering nodes with [%s]' % ', '.join(
+            '"%s"' % r for r in self.regex_list))
+
+        cluster_name = '-'.join('/%s/' % r for r in self.regex_list)
+        logger.debug("Cluster name: %s", cluster_name)
+
+        cluster_ids = OrderedSet()
         for regex_str in self.regex_list:
-            logger.info("Clustering nodes with /%s/...", regex_str)
+            logger.debug("Adding nodes with /%s/...", regex_str)
 
-            cluster_ids = OrderedSet()
             compiled = re.compile(regex_str)
-            for node_id, long_name in dep_graph.nodes('long_name'):
-                if compiled.search(long_name):
+            for node_id in dep_graph.nodes:
+                if compiled.search(node_id):
                     cluster_ids.add(node_id)
-            if len(cluster_ids) > 0:
-                subgraph = dep_graph.__class__()
-                subgraph.add_nodes_from(cluster_ids)
-                subgraph.add_edges_from(
-                    (u, v) for (u, v) in dep_graph.edges()
-                    if u in subgraph if v in subgraph)
 
-                cluster = Cluster(regex_str, subgraph)
-                dep_graph.clusters.add(cluster)
+        if len(cluster_ids) > 0:
+            logger.info('Cluster contains %d nodes.', len(cluster_ids))
 
-            logger.info("Done")
+            subgraph = dep_graph.__class__()
+            subgraph.add_nodes_from(cluster_ids)
+            subgraph.add_edges_from(
+                (u, v) for (u, v) in dep_graph.edges()
+                if u in subgraph if v in subgraph)
 
+            cluster = Cluster(cluster_name, subgraph)
+            dep_graph.clusters.add(cluster)
+        else:
+            logger.info('Empty cluster: %s', cluster_name)
+
+        logger.info("Done")
         return dep_graph
